cc21551a2d4b0c235e08a8b315adfc801c45631b
mariam-toure <mariam.toure@laplateforme.io>
2022-11-16 19:04:36 +0100

    create

---
+++ jour01/job01/index.php
@@ -0,0 +1,15 @@
+<?php
+$str3 = 'Vive Laplateforme!';
+echo $str3;
+?>
+<?php
+$val = 6;
+echo ($val + 4);
+?>
+<?php
+$mybool =true;
+echo $mybool;
+$mybool = false;
+echo $mybool;
+?>
+ 
---
+++ jour01/job02/index.php
@@ -0,0 +1,8 @@
+<?php
+ 
+echo 'Hello Laplateforme'; //Ma premiere ligne
+
+echo 'Hello La Plateforme'; /* Ma bloc 
+commentaire
+a plusieur lignes*/
+?>
